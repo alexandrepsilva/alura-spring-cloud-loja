8677522b3c9a7960da7edfb4eb16d74f214f96e0
apasilva <alexandre.palota@gmail.com>
2021-05-06 11:58:50 -0300

    Adicionado Lombok aos DTO

--- src/main/java/br/com/alura/microservice/loja/dto/CompraDTO.java
+++ src/main/java/br/com/alura/microservice/loja/dto/CompraDTO.java
@@ -1,25 +1,15 @@
 package br.com.alura.microservice.loja.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class CompraDTO {
 
     private List<ItemDaCompraDTO> itens;
     private EnderecoDTO endereco;
 
-    public List<ItemDaCompraDTO> getItens() {
-        return itens;
-    }
-
-    public void setItens(List<ItemDaCompraDTO> itens) {
-        this.itens = itens;
-    }
-
-    public EnderecoDTO getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(EnderecoDTO endereco) {
-        this.endereco = endereco;
-    }
 }
--- src/main/java/br/com/alura/microservice/loja/dto/ItemDaCompraDTO.java
+++ src/main/java/br/com/alura/microservice/loja/dto/ItemDaCompraDTO.java
@@ -1,22 +1,12 @@
 package br.com.alura.microservice.loja.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ItemDaCompraDTO {
     private long id;
     private int quantidade;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
 }
